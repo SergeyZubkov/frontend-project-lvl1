d14bed5b23cd56cb1bfd96f138b8ef55ebb68696
Sergey Zubkov <svzubkovru@gmail.com>
2022-04-30 10:08:52 +0300

    v2

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greetings = () => {
+	const name = readlineSync.question('What you name?\n');
+
+	console.log(`Hello, ${name}`)
+}
+
+export default greetings;
